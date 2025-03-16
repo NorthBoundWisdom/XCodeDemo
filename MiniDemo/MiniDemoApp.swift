e92c045b10dbf791692c6d050d97d3354395c025
EthanKang <kangyucheng1998@outlook.com>
2025-03-16 22:23:25 +0800

    Initial Commit

---
+++ MiniDemo/MiniDemoApp.swift
@@ -0,0 +1,32 @@
+//
+//  MiniDemoApp.swift
+//  MiniDemo
+//
+//  Created by Henry Kang on 2025/3/16.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MiniDemoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ MiniDemoTests/MiniDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  MiniDemoTests.swift
+//  MiniDemoTests
+//
+//  Created by Henry Kang on 2025/3/16.
+//
+
+import Testing
+
+struct MiniDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
