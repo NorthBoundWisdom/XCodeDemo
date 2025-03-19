135bc7fb47f4c22fa05ad5b18074d87028d23759
HenryKang <kangyucheng1998@outlook.com>
2025-03-19 16:09:58 +0800

    update

--- MiniDemo/Item.swift
+++ MiniDemo/Item.swift
@@ -1,17 +1,10 @@
-//
-//  Item.swift
-//  MiniDemo
-//
-//  Created by Henry Kang on 2025/3/16.
-//
-
 import Foundation
 import SwiftData
 
 @Model
 final class Item {
     var timestamp: Date
-    
+
     init(timestamp: Date) {
         self.timestamp = timestamp
     }
